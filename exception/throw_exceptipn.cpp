bee932b55b856bd780faffe647ba186e8be9e7da
BenVanke <hsfbivanke@163.com>
2021-08-21 15:16:19 +0800

    throw 异常学习

---
+++ exception/throw_exceptipn.cpp
@@ -0,0 +1,43 @@
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+void throw_exception1()
+{
+    throw true;
+    /* 抛出异常后, 后续程序不再执行 */    
+    cout << "throw_exception1 after throw true;" << endl;
+}
+
+/* 加了noexcept 修饰之后, 本来是抛出异常的, 现在直接异常---程序终止 */
+void throw_exception2() noexcept
+{
+    throw true;
+    /* 抛出异常后, 后续程序不再执行 */
+    cout << "throw_exception2 after throw true;" << endl;    
+}
+
+int main()
+{
+    cout << "let's begin" << endl;
+
+    /* 如果抛出异常但是没有相应的catch, 程序一样会异常终止 */
+    //throw_exception1();
+
+    try {
+        throw_exception1();
+    } catch (...) {
+        cout << "catch exception......." << endl;
+    };
+
+    try {
+        throw_exception2();
+    } catch (...) {
+        cout << "catch exception......." << endl;
+    };
+
+    cout << "it's end" << endl;
+
+    return 0;
+}
